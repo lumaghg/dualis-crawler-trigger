8d10021039ca0a28acf7f81c898032c17ceccf5d
lumaghg <rotkehlchen567@gmail.com>
2021-10-07 23:53:23 +0200

    added invocation of other lambda

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"sync"
 
@@ -10,6 +12,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
+	lambdaService "github.com/aws/aws-sdk-go/service/lambda"
 )
 
 const (
@@ -17,9 +20,9 @@ const (
 )
 
 type User struct {
-	Email             string `json:"email"`
-	Password          string `json:"password"`
-	NotificationEmail string `json:"notificationEmail"`
+	Email             string
+	Password          string
+	NotificationEmail string
 }
 
 func HandleRequest() error {
@@ -32,6 +35,7 @@ func HandleRequest() error {
 
 	// Create DynamoDB client
 	dynamoClient := dynamodb.New(sess)
+	lambdaClient := lambdaService.New(sess, &aws.Config{Region: aws.String("eu-central-1")})
 	//get all the users and invoke the scrapeGradesAndNotify function
 	proj := expression.NamesList(expression.Name("Email"), expression.Name("Password"), expression.Name("NotificationEmail"))
 
@@ -56,21 +60,40 @@ func HandleRequest() error {
 		return err
 	}
 
+	wg := sync.WaitGroup{}
+	errChan := make(chan error, 1)
 	for _, userMap := range result.Items {
-		user := User{}
-		err = dynamodbattribute.UnmarshalMap(userMap, &user)
-		if err != nil {
-			log.Fatalf("Got error unmarshalling: %s", err)
-			return err
-		}
-		waitGroup := sync.WaitGroup{}
-		go func(user User) {
-			waitGroup.Add(1)
 
-		}(user)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			user := User{}
+			err = dynamodbattribute.UnmarshalMap(userMap, &user)
+			if err != nil {
+				log.Fatalf("Got error unmarshalling: %s", err)
+				errChan <- err
+			}
+
+			payload, err := json.Marshal(user)
+			if err != nil {
+				log.Fatal(err)
+				errChan <- err
+			}
+
+			_, err = lambdaClient.Invoke(&lambdaService.InvokeInput{FunctionName: aws.String("triggerScrapeGradesAndNotify"), Payload: payload})
+			if err != nil {
+				fmt.Println("Error calling MyGetItemsFunction")
+				errChan <- err
+			}
+		}()
+
 	}
-	//TODO invoke other function
 
+	wg.Wait()
+	if len(errChan) > 0 {
+		return err
+	}
 	return nil
 }
 
